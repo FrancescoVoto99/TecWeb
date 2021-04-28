49f93c7a7d68ccc919a17b7e2eb5e5c993a17714
Gianluca Ciarlariello <ciarlariello47@gmail.com>
2021-04-28 17:31:41 +0200

    tabella

--- 2059_curtain_store/eventi.php
+++ 2059_curtain_store/eventi.php
@@ -9,17 +9,27 @@
             <tr><th>Nome</th><th>Data</th><th>luogo</th><th>Prezzo</th><th>biglietti</th></tr>
             <?php
               include("include/connessione.php");
+              
               $conn=mysqli_connect($HOST, $USER, $PASSWORD,$DB);
-              $ris=mysqli_query($conn, "select * from evento");
+              $ris=mysqli_query($conn, "select * from eventi");
+              
 			  while ($row=mysqli_fetch_assoc($ris)) {
-				$co=$row["Nome Evento"];
-				$ti=$row["Data Evento"];
-				$au=$row["Luogo Evento"];
-				$pr=$row["Prezzo Evento"];
-				$dv=$row["Biglietti venduti"];
-                echo "<tr><td>$co</td><td>$ti</td><td>$au</td><td>$pr</td><td>$dv</td></tr>";
-             
-                }
+                                $id=$row["id"];
+				$no=$row["nomeEvento"];
+				$de=$row["descrizione"];
+                                //header("Content-Type:/jpeg ");
+                                //$da=$row["dati"];
+				$do=$row["dataOra"];
+				$lu=$row["luogo"];
+                                $ca=$row["categoria"];
+                                $ra=$row["raggiungere"];
+                                $pr=$row["prezzo"];
+                                $di=$row["bigliettiDisponibili"];
+                                $ve=$row["bigliettiVenduti"];
+                                $to=$row["IncassoTotale"];
+                                $sc=$row["sconto"];
+                /*echo "<tr><td>$id</td><td>$no</td><td>$de</td><td><$da></td><td>$do</td><td>$lu</td><td>$ca</td><td>$ra</td><td>$pr</td><td>$di</td><td>$ve</td><td>$to</td><td>$sc</td></tr>";
+                }*/
 			  mysqli_free_result($ris);
               mysqli_close($conn);
             ?>

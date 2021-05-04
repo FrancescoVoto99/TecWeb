55fa0fe68dfc33024a980b6060dba92cea1b3699
Gianluca Ciarlariello <ciarlariello47@gmail.com>
2021-05-04 12:06:24 +0200

    immagini

--- 2059_curtain_store/home12.php
+++ 2059_curtain_store/home12.php
@@ -23,42 +23,35 @@
             </div>
 
             <div id="tooplate_main">
-                   <div id="tooplate_sidebar">
+            <div id="tooplate_sidebar">
             <div class="sb_box">
-            <h3>CATEGORIE</h3>      
-            
-         
-           <form action="home12.php" method="post" >
-           <fieldset title="Scegli una categoria">
-          <label><input type="checkbox" name="cate[]" value="calcio" >Calcio </label><br></br>	
-          <label><input type="checkbox" name= "cate[]" value="basket">Basket</label><br></br>
-          <label><input type="checkbox" name= "cate[]" value="pallavolo">Pallavolo </label><br></br>
-         <input type="submit" value="Invia">
-            </fieldset>
-         
-     </form>        
-         </div>
-          </div> 
+                <h3>CATEGORIE</h3>      
+                <form action="home12.php" method="post" >
+                <fieldset title="Scegli una categoria">
+                <label><input type="checkbox" name="cate[]" value="calcio" >Calcio </label><br></br>
+                <label><input type="checkbox" name= "cate[]" value="basket">Basket</label><br></br>
+                <label><input type="checkbox" name= "cate[]" value="pallavolo">Pallavolo</label><br></br>
+                <input type="submit" value="Invia">
+                </fieldset>
+                </form>
+            </div>
+            </div>
                
-                <?php
-              include("include/connessione.php");
-              $sql= "select * from eventi ";
-               $stringa="";
-             if(isset($_POST["cate"])){
-             for($i=0;$i<sizeof($_POST["cate"]);$i++)
-             {
-                if($i==(sizeof($_POST["cate"])-1))
+            <?php
+                include("include/connessione.php");
+                $sql= "select * from eventi ";
+                $stringa="";
+                if(isset($_POST["cate"])){
+                for($i=0;$i<sizeof($_POST["cate"]);$i++){
+                    if($i==(sizeof($_POST["cate"])-1))
                     $stringa .= "categoria = '" . $_POST["cate"][$i] . "'" ;
-         else
-            $stringa .= "categoria = '" . $_POST["cate"][$i] . "' or ";
-    }
-            $sql .= "where " . $stringa;
-             }
-             
-             
-               
-                  
-                    $conn=mysqli_connect($HOST, $USER, $PASSWORD,$DB);
+                else
+                    $stringa .= "categoria = '" . $_POST["cate"][$i] . "' or ";}
+                $sql .= "where " . $stringa;
+                }
+                
+                
+                $conn=mysqli_connect($HOST, $USER, $PASSWORD,$DB);
                     $ris=mysqli_query($conn, $sql);
                     while ( $row=mysqli_fetch_assoc($ris)) {
                             $id=$row["id"];
@@ -69,49 +62,37 @@
                                 //$sc=$row["sconto"];
                
                       
-              echo ' <div id="tooplate_content">';
-                 echo "<h2><a name='$id' href='evento.php?id=$id'>$no</a></h2>";
-                  echo '  <img src="images/tooplate_image_04.png" alt="Image 04" />';
-                  echo '  <div class="latofoto">';
-                   echo " <p>Prezzo: $pr</p>";
-                   echo ' <br></br>';
-                  echo " <p>Biglietti disponibili: $di </p>";
-                  echo '  </div>';
-                 echo '   <br></br><br></br>';
+                echo ' <div id="tooplate_content">';
+                echo "<h2><a name='$id' href='evento.php?id=$id'>$no</a></h2>";
+                echo '<img src="images/tooplate_image_04.png" alt="Image 04" />';
+                echo '<div class="latofoto">';
+                echo "<p>Prezzo: $pr</p>";
+                echo '<br></br>';
+                echo "<p>Biglietti disponibili: $di </p>";
+                echo '</div>';
+                echo '<br></br><br></br>';
+                echo "<p>$de</p>";
+                echo '<div class="cleaner h20"></div>';
+                echo '<br class="cleaner" />';
+                echo '<hr> </hr>';
+                echo '</div>';
+                    }
                     
-                    
-                echo "    <p>$de</p>";
-                        
-                 echo '   <div class="cleaner h20"></div>';
-                    
-                echo '    <br class="cleaner" />';
-              echo '<hr> </hr>';
-                        echo '  </div>';
-              
-              }
-               mysqli_free_result($ris);
-              mysqli_close($conn);
+                mysqli_free_result($ris);
+                mysqli_close($conn);
             ?>
-                
-                
-
-
-                <div class="cleaner"></div>
-            </div> 
-            <!-- end of main -->
-
             <div class="cleaner"></div>
-        
-        <div id="tooplate_footer">
+            </div>  <!-- end of main -->
+            <div class="cleaner"></div>
+            <div id="tooplate_footer">
             <footer>
                 
                 <p class="footer1"><a href="FAQ.html">FAQ</a></p>
-                <p class="footer1" ><a href="chi_siamo.html">Chi Siamo</a></p>
+                <p class="footer1"><a href="chi_siamo.html">Chi Siamo</a></p>
                 <p class="footer1"><a href="lavora_con_noi.html">Lavora Con Noi</a></p>
-                
-                 
-                Copyright © 2048 Company Name </footer> 
-        </div>
+                Copyright © 2048 Company Name 
+            </footer> 
+            </div>
         </div> <!-- end of main -->
     </body>
 </html>
